fc7f10646fa60ece1d09f48db545a6a89d556253
Samuel Leong <leowhiz@yahoo.com.sg>
2020-05-19 12:36:37 +0800

    Increase point size

--- mvn/utils/vis.py
+++ mvn/utils/vis.py
@@ -176,7 +176,7 @@ def visualize_batch(images_batch, heatmaps_batch, keypoints_2d_batch, proj_matri
         keypoints_2d = to_numpy(keypoints_2d_batch)[batch_index]
         for view_i in range(n_cols):
             axes[row_i][view_i].imshow(images[view_i])
-            draw_2d_pose(keypoints_2d[view_i], axes[row_i][view_i], kind=kind, line_width=2, point_size=3)
+            draw_2d_pose(keypoints_2d[view_i], axes[row_i][view_i], kind=kind, line_width=2, point_size=4)
         row_i += 1
 
     # 2D keypoints (gt projected)
@@ -185,7 +185,7 @@ def visualize_batch(images_batch, heatmaps_batch, keypoints_2d_batch, proj_matri
     for view_i in range(n_cols):
         axes[row_i][view_i].imshow(images[view_i])
         keypoints_2d_gt_proj = project_3d_points_to_image_plane_without_distortion(proj_matricies_batch[batch_index, view_i].detach().cpu().numpy(), keypoints_3d_batch_gt[batch_index].detach().cpu().numpy())
-        draw_2d_pose(keypoints_2d_gt_proj, axes[row_i][view_i], kind=kind, line_width=2, point_size=3)
+        draw_2d_pose(keypoints_2d_gt_proj, axes[row_i][view_i], kind=kind, line_width=2, point_size=4)
     row_i += 1
 
     # 2D keypoints (pred projected)
@@ -194,7 +194,7 @@ def visualize_batch(images_batch, heatmaps_batch, keypoints_2d_batch, proj_matri
     for view_i in range(n_cols):
         axes[row_i][view_i].imshow(images[view_i])
         keypoints_2d_pred_proj = project_3d_points_to_image_plane_without_distortion(proj_matricies_batch[batch_index, view_i].detach().cpu().numpy(), keypoints_3d_batch_pred[batch_index].detach().cpu().numpy())
-        draw_2d_pose(keypoints_2d_pred_proj, axes[row_i][view_i], kind=pred_kind, line_width=2, point_size=3)
+        draw_2d_pose(keypoints_2d_pred_proj, axes[row_i][view_i], kind=pred_kind, line_width=2, point_size=4)
     row_i += 1
 
     # cuboids
