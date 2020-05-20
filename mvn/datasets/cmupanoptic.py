fe32ebc108ce4d7057911c701bea4535082998b1
Samuel Leong <leowhiz@yahoo.com.sg>
2020-05-20 15:47:30 +0800

    Debug 2

--- mvn/datasets/cmupanoptic.py
+++ mvn/datasets/cmupanoptic.py
@@ -130,7 +130,7 @@ class CMUPanopticDataset(Dataset):
                 
                 indices.extend(np.nonzero(mask)[0])
 
-            indices = [np.array(indices)]
+            # indices = [np.array(indices)]
         else:
             train_actions = [
                 "171026_pose3", "171026_pose2", "171026_pose1", "171204_pose4",
