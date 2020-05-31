1ada42d10f4424ee906ac3c57d4efb308e6d10e1
Samuel Leong <leowhiz@yahoo.com.sg>
2020-05-31 20:58:09 +0800

    Training

--- train.py
+++ train.py
@@ -712,7 +712,7 @@ def main(args):
             if DEBUG:
                 print(f"Training epoch {epoch}...")
 
-            if not DEBUG:
+            if True: # not DEBUG:
                 n_iters_total_train = one_epoch(model, criterion, opt, config, train_dataloader, device, epoch, n_iters_total=n_iters_total_train, is_train=True, master=master, experiment_dir=experiment_dir, writer=writer)
 
             if DEBUG:
