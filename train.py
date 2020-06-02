309d4e9c141b7d5ebf51d4114087a7a1822f3f22
Samuel Leong <leowhiz@yahoo.com.sg>
2020-06-02 10:51:34 +0800

    Empty cuda cache

--- train.py
+++ train.py
@@ -718,6 +718,9 @@ def main(args):
             global training_complete
             training_complete = False
 
+            # Cache needs to be emptied first
+            torch.cuda.empty_cache()
+
             n_iters_total_train = one_epoch(model, criterion, opt, config, train_dataloader, device, epoch, n_iters_total=n_iters_total_train, is_train=True, master=master, experiment_dir=experiment_dir, writer=writer)
 
             '''
