0318e29d7e420825292cc1a5b585623235583681
Samuel Leong <leowhiz@yahoo.com.sg>
2020-06-08 19:26:14 +0800

    oops

--- scripts/train_cmu_handle_error.sh
+++ scripts/train_cmu_handle_error.sh
@@ -14,9 +14,11 @@ WRITE_TO_USER=scleong
 git pull && ./scripts/clear_nvidia_cache
 
 for epoch in {$OFFSET..$END}
+do
     git pull && ./clear_nvidia_cache
     ./train_cmu 3 || {
         cd ./logs/cmu/train
         ./change_checkpoints cmu_vol_softmax_VolumetricTriangulationNet@* $epoch
         echo "Epoch $epoch complete!" | write $WRITE_TO_USER
     }
+done
