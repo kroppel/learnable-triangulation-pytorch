22df7751a3759178d2369945f073df0e42b3b718
Samuel Leong <leowhiz@yahoo.com.sg>
2020-06-18 22:02:25 +0800

    Issue with keypoints

--- train.py
+++ train.py
@@ -339,7 +339,7 @@ def one_epoch(model, criterion, opt, config, dataloader, device, epoch, n_iters_
                 # force ground truth keypoints to fit config kind
                 keypoints_gt_original = keypoints_3d_gt.clone()
 
-                if keypoints_3d_gt.shape[1] != n_joints and transfer_cmu_h36m:
+                if keypoints_3d_gt.shape[1] != n_joints : #and transfer_cmu_h36m:
                     print(
                         f"[Warning] Possibly due to different pretrained model type, ground truth has {keypoints_3d_gt.shape[1]} keypoints while predicted has {n_joints} keypoints"
                     )
